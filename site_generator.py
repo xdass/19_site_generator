9b7755ee304270006abe01fb1c440a3ac833b220
Dass <420600@gmail.com>
2017-11-27 00:03:36 +0500

    Move create paths to function

--- site_generator.py
+++ site_generator.py
@@ -1,7 +1,9 @@
-import jinja2
-import markdown
 import os
 import json
+from collections import defaultdict
+import jinja2
+import markdown
+
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader('templates')
@@ -25,19 +27,34 @@ def load_json_config(json_file):
 
 
 def create_articles_catalog():
+    #dd = defaultdict(list)
     dict_config = load_json_config(';;;')
     topics_dir = 'topics'
-    html_prfix = '.html'
+    html_prefix = '.html'
     if not os.path.exists(topics_dir):
         os.mkdir(topics_dir)
     for article in dict_config['articles']:
-        path = os.path.join(topics_dir, os.path.split(article['source'])[0])
-        os.makedirs(path, exist_ok=True)
-        html = convert_markdown_to_html(os.path.abspath(os.path.join('articles', article['source'])))
-        topic_path = os.path.splitext(article['source'])[0]
-        full_path = os.path.abspath(os.path.join(topics_dir, topic_path + html_prfix))
-        with open(os.path.abspath(full_path), 'w', encoding='utf-8') as file:
-            file.write(html)
+        #dir_path = os.path.join(topics_dir, os.path.split(article['source'])[0])
+        print(create_paths(article, topics_dir))
+        #os.makedirs(dir_path, exist_ok=True)
+        #path_to_md_file = os.path.abspath(os.path.join('articles', article['source']))
+        #html = convert_markdown_to_html(path_to_md_file)
+        #md_filename = os.path.split(path_to_md_file)[1]
+        #html_filename = os.path.splitext(md_filename)[0] + html_prefix
+        #path_to_html_file = os.path.join(*(os.getcwd(), dir_path, html_filename))
+        #dd[article['topic']].append(path_to_html_file)
+        # with open(os.path.abspath(path_to_html_file), 'w', encoding='utf-8') as file:
+        #     file.write(html)
+
+
+def create_paths(article, output_dir):
+    html_prefix = '.html'
+    dir_path = os.path.join(output_dir, os.path.split(article['source'])[0])
+    path_to_md_file = os.path.abspath(os.path.join('articles', article['source']))
+    md_filename = os.path.split(path_to_md_file)[1]
+    html_filename = os.path.splitext(md_filename)[0] + html_prefix
+    path_to_html_file = os.path.join(*(os.getcwd(), dir_path, html_filename))
+    return output_dir, path_to_md_file, path_to_html_file
 
 
 def create_index():
@@ -66,5 +83,4 @@ def create_index():
     print(r)
 
 create_articles_catalog()
-
 
