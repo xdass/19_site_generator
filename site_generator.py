794824a6e647113b2964f7918a4b0df5ef0c1399
Dass <420600@gmail.com>
2017-11-28 23:39:57 +0500

    Update main logic

--- site_generator.py
+++ site_generator.py
@@ -3,6 +3,7 @@ import json
 from collections import defaultdict
 import jinja2
 import markdown
+import shutil
 
 
 env = jinja2.Environment(
@@ -27,60 +28,51 @@ def load_json_config(json_file):
 
 
 def create_articles_catalog():
-    #dd = defaultdict(list)
+    dd = defaultdict(list)
     dict_config = load_json_config(';;;')
-    topics_dir = 'topics'
-    html_prefix = '.html'
-    if not os.path.exists(topics_dir):
-        os.mkdir(topics_dir)
     for article in dict_config['articles']:
-        #dir_path = os.path.join(topics_dir, os.path.split(article['source'])[0])
-        print(create_paths(article, topics_dir))
-        #os.makedirs(dir_path, exist_ok=True)
-        #path_to_md_file = os.path.abspath(os.path.join('articles', article['source']))
-        #html = convert_markdown_to_html(path_to_md_file)
-        #md_filename = os.path.split(path_to_md_file)[1]
-        #html_filename = os.path.splitext(md_filename)[0] + html_prefix
-        #path_to_html_file = os.path.join(*(os.getcwd(), dir_path, html_filename))
-        #dd[article['topic']].append(path_to_html_file)
-        # with open(os.path.abspath(path_to_html_file), 'w', encoding='utf-8') as file:
-        #     file.write(html)
+        html_file_path, md_file_path = create_paths(article)
+        html = convert_markdown_to_html(md_file_path)
+        dd[article['topic']].append(html_file_path)
+        with open(html_file_path, 'w', encoding='utf-8') as file:
+            file.write(html)
+    print(dd)
 
 
-def create_paths(article, output_dir):
+def create_paths(article):
     html_prefix = '.html'
-    dir_path = os.path.join(output_dir, os.path.split(article['source'])[0])
-    path_to_md_file = os.path.abspath(os.path.join('articles', article['source']))
+    path_to_md_file = os.path.join(config['paths']['md_articles'], article['source'])
     md_filename = os.path.split(path_to_md_file)[1]
     html_filename = os.path.splitext(md_filename)[0] + html_prefix
-    path_to_html_file = os.path.join(*(os.getcwd(), dir_path, html_filename))
-    return output_dir, path_to_md_file, path_to_html_file
+    path_to_html_file = os.path.join(
+        config['paths']['root'],
+        config['paths']['html_articles'],
+        os.path.split(article['source'])[0],
+        html_filename
+    )
+    return path_to_html_file, path_to_md_file
+
+
+def create_site_structure(articles, paths):
+    topics_path = os.path.join(config['paths']['root'], config['paths']['md_articles'])
+    root = config['paths']['root']
+    css_path = os.path.join(root, config['paths']['css'])
+    js_path = os.path.join(root, config['paths']['js'])
+    site_css = ''
+    site_js = ''
+    try:
+        shutil.copytree(config['paths']['css'], css_path)
+        shutil.copytree(config['paths']['js'], js_path)
+        for article in config['articles']:
+            dirs_path = os.path.split(article['source'])[0]
+            os.makedirs(os.path.join('site/topics', dirs_path), exist_ok=True)
+    except FileExistsError:
+        pass
 
 
 def create_index():
     index_template = env.get_template('index.html')
-    r = index_template.render(
-        {
-            'topics': [
-                {
-                    "slug": "tutorial",
-                    "title": "Арсенал"
-                },
-                {
-                    "slug": "python_basics",
-                    "title": "Основы Питона"
-                },
-                {
-                    "slug": "html",
-                    "title": "HTML"
-                },
-                {
-                    "slug": "git",
-                    "title": "Гит"
-                }
-            ]
-        })
-    print(r)
 
+config = load_json_config('')
+create_site_structure('', '')
 create_articles_catalog()
-
