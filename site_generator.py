5a58db1191e05061370b1f23a7500a8e561b50e6
xdass <420600@gmail.com>
2017-11-29 16:01:10 +0500

    Update functionality

--- site_generator.py
+++ site_generator.py
@@ -33,10 +33,9 @@ def create_articles_catalog():
     for article in dict_config['articles']:
         html_file_path, md_file_path = create_paths(article)
         html = convert_markdown_to_html(md_file_path)
-        dd[article['topic']].append(html_file_path)
-        with open(html_file_path, 'w', encoding='utf-8') as file:
-            file.write(html)
-    print(dd)
+        dd[article['topic']].append([article['title'], html_file_path])
+        create_html_page(html, html_file_path)
+    return dd
 
 
 def create_paths(article):
@@ -45,7 +44,7 @@ def create_paths(article):
     md_filename = os.path.split(path_to_md_file)[1]
     html_filename = os.path.splitext(md_filename)[0] + html_prefix
     path_to_html_file = os.path.join(
-        config['paths']['root'],
+        #config['paths']['root'],
         config['paths']['html_articles'],
         os.path.split(article['source'])[0],
         html_filename
@@ -54,7 +53,10 @@ def create_paths(article):
 
 
 def create_site_structure(articles, paths):
-    topics_path = os.path.join(config['paths']['root'], config['paths']['md_articles'])
+    topics_path = os.path.join(
+        config['paths']['root'],
+        config['paths']['md_articles']
+    )
     root = config['paths']['root']
     css_path = os.path.join(root, config['paths']['css'])
     js_path = os.path.join(root, config['paths']['js'])
@@ -70,9 +72,21 @@ def create_site_structure(articles, paths):
         pass
 
 
-def create_index():
-    index_template = env.get_template('index.html')
+def create_html_page(html, html_filepath):
+    page_template = env.get_template('template.html')
+    html_page = page_template.render({'html': html})
+    print(os.path.join('site',html_filepath))
+    with open(os.path.join('site', html_filepath), 'w', encoding='utf-8') as file:
+        file.write(html_page)
+
+
+def create_index_page(dd):
+    index_template = env.get_template('index-template.html')
+    html = index_template.render({'title': 'Энциклопедия', 'topics': dd, 'config': config['topics']})
+    with open('site/index.html', 'w', encoding='utf-8') as file:
+        file.write(html)
 
 config = load_json_config('')
 create_site_structure('', '')
-create_articles_catalog()
+stat = create_articles_catalog()
+create_index_page(stat)
